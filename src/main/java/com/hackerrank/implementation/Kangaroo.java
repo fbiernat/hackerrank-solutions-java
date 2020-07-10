7bfa585e92236738958e822a8d3c1dad07f79816
fbiernat <filipbiernat@gmail.com>
2020-07-10 18:08:18 +0200

    Add new solutions

--- src/main/java/com/hackerrank/implementation/Kangaroo.java
+++ src/main/java/com/hackerrank/implementation/Kangaroo.java
@@ -8,7 +8,72 @@ public class Kangaroo {
     }
 
     static String kangaroo(int x1, int v1, int x2, int v2) {
+
+        KangarooSpecimen kangaroo1 = new KangarooSpecimen(x1, v1);
+        KangarooSpecimen kangaroo2 = new KangarooSpecimen(x2, v2);
+
+        if (kangaroo1.getPosition() < kangaroo2.getPosition() && kangaroo1.getVelocity() > kangaroo2.getVelocity() ||
+                kangaroo1.getPosition() > kangaroo2.getPosition() && kangaroo1.getVelocity() < kangaroo2.getVelocity()) {
+
+            if (kangaroo1.getPosition() > kangaroo2.getPosition()) {
+                while (kangaroo1.getPosition() > kangaroo2.getPosition()) {
+                    kangaroo1.jump();
+                    kangaroo2.jump();
+                }
+                if (kangaroo1.getPosition() == kangaroo2.getPosition()) {
+                    return "YES";
+                } else {
+                    return "NO";
+                }
+            }
+            if (kangaroo1.getPosition() < kangaroo2.getPosition()) {
+                while (kangaroo1.getPosition() < kangaroo2.getPosition()) {
+                    kangaroo1.jump();
+                    kangaroo2.jump();
+                }
+                if (kangaroo1.getPosition() == kangaroo2.getPosition()) {
+                    return "YES";
+                } else {
+                    return "NO";
+                }
+            }
+        } else {
+            // kangaroo further away has greater velocity than the closer one
+            return "NO";
+        }
+
         return "NO";
+    }
+
+    static class KangarooSpecimen {
+
+        private int position;
+        private int velocity;
+
+        KangarooSpecimen(int position, int velocity) {
+            this.position = position;
+            this.velocity = velocity;
+        }
+
+        public int getPosition() {
+            return position;
+        }
+
+        public void setPosition(int position) {
+            this.position = position;
+        }
+
+        public int getVelocity() {
+            return velocity;
+        }
+
+        public void setVelocity(int velocity) {
+            this.velocity = velocity;
+        }
+
+        public void jump() {
+            this.position += this.velocity;
+        }
 
     }
 
