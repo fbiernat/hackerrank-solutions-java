61b59a180d251eef27d1253b68b029a3fe4395ed
fbiernat <filipbiernat@gmail.com>
2020-07-28 16:10:26 +0200

    Add Beautiful Days At The Movies test

---
+++ src/test/java/com/hackerrank/implementation/BeautifulDaysAtTheMoviesTest.java
@@ -0,0 +1,13 @@
+package com.hackerrank.implementation;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BeautifulDaysAtTheMoviesTest {
+
+    @Test
+    public void testBeautifulDaysAtTheMovies() {
+        Assert.assertEquals(2, BeautifulDaysAtTheMovies.beautifulDays(20, 23, 6));
+    }
+    
+}
